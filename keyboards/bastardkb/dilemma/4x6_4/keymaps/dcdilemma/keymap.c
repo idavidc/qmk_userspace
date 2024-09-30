abb8991ddaed1e72d59a0c82eec5bb64199a604d
idavidc <idirectorschair@gmail.com>
2024-09-30 16:48:45 -0500

    enable debug

--- keyboards/bastardkb/dilemma/4x6_4/keymaps/dcdilemma/keymap.c
+++ keyboards/bastardkb/dilemma/4x6_4/keymaps/dcdilemma/keymap.c
@@ -35,6 +35,16 @@ void matrix_scan_user(void) {
     }
 };
 
+// Enablng debug
+
+void keyboard_post_init_user(void) {
+  // Customise these values to desired behaviour
+  debug_enable=true;
+  debug_matrix=true;
+  //debug_keyboard=true;
+  //debug_mouse=true;
+}
+
 // Tap Dance
 enum {
     TD_CTL_GUI,
