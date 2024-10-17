cce094cf2b1df83f1b02b05631f09390f4189e47
idavidc <idirectorschair@gmail.com>
2024-10-17 12:48:49 -0500

    reg 16

--- keyboards/bastardkb/dilemma/4x6_4/keymaps/dcdilemma/keymap.c
+++ keyboards/bastardkb/dilemma/4x6_4/keymaps/dcdilemma/keymap.c
@@ -268,8 +268,8 @@ void x_finished(tap_dance_state_t *state, void *user_data) {
     xtap_state.state = cur_dance(state);
     switch (xtap_state.state) {
         case TD_SINGLE_TAP: register_code(KC_X); break;
-        case TD_SINGLE_HOLD: SEND_STRING(SS_RCTL(SS_TAP(X_HOME))); break;
-        case TD_DOUBLE_TAP: register_code(KC_ESC); break;
+        case TD_SINGLE_HOLD: register_code16(RCTL(X_HOME)); break;
+        case TD_DOUBLE_TAP: register_code(RCTL(X_HOME)); break;
         case TD_DOUBLE_HOLD: register_code(KC_LALT); break;
         // Last case is for fast typing. Assuming your key is `f`:
         // For example, when typing the word `buffer`, and you want to make sure that you send `ff` and not `Esc`.
@@ -282,7 +282,7 @@ void x_finished(tap_dance_state_t *state, void *user_data) {
 void x_reset(tap_dance_state_t *state, void *user_data) {
     switch (xtap_state.state) {
         case TD_SINGLE_TAP: unregister_code(KC_X); break;
-        case TD_SINGLE_HOLD: unregister_code(KC_RCTL); unregister_code(KC_HOME); break;
+        case TD_SINGLE_HOLD: unregister_code16(RCTL(X_HOME)); break;
         case TD_DOUBLE_TAP: unregister_code(KC_ESC); break;
         case TD_DOUBLE_HOLD: unregister_code(KC_LALT); break;
         case TD_DOUBLE_SINGLE_TAP: unregister_code(KC_X); break;
