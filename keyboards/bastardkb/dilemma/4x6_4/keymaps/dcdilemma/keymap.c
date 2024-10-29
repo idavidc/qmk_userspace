4874c1045c5c5ea69ef60e787912e5b007e354cd
idavidc <idirectorschair@gmail.com>
2024-10-29 11:29:27 -0500

    moved enum

--- keyboards/bastardkb/dilemma/4x6_4/keymaps/dcdilemma/keymap.c
+++ keyboards/bastardkb/dilemma/4x6_4/keymaps/dcdilemma/keymap.c
@@ -302,13 +302,11 @@ tap_dance_action_t tap_dance_actions[] = {
     // Tap once for Escape, twice for Caps Lock
     [TD_CTL_GUI] = ACTION_TAP_DANCE_DOUBLE(QK_MAGIC_SWAP_LCTL_LGUI, QK_MAGIC_UNSWAP_LCTL_LGUI),
     [TD_HOMEZ] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, x_finished, x_reset),
-    [TD_TEST_1] = ACTION_TAP_DANCE_FN_ADVANCED_USER(NULL, test_fin, NULL, &((test_user_data_t)(KC_A))),
-    [TD_TEST_2] = ACTION_TAP_DANCE_FN_ADVANCED_USER(NULL, test_fin, NULL, &((test_user_data_t)(KC_B))),
+    [TD_TEST_1] = ACTION_TAP_DANCE_FN_ADVANCED_USER(NULL, test_fin, NULL, (test_user_data_t)(KC_A)),
+    [TD_TEST_2] = ACTION_TAP_DANCE_FN_ADVANCED_USER(NULL, test_fin, NULL, (test_user_data_t)(KC_B)),
 };
 
 // Dynamic TD Handling
-
-
 
 void test_fin(tap_dance_state_t *state, void *user_data) {
     uint16_t keycode = ((test_user_data_t*)user_data)->keycode;
