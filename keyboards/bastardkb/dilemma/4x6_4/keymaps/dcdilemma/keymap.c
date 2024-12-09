cc88f3e7b7be6405f484045ee62ba13b6f7fcca5
David Chen <davidchen@chevron.com>
2024-12-09 16:26:55 -0600

    typo

--- keyboards/bastardkb/dilemma/4x6_4/keymaps/dcdilemma/keymap.c
+++ keyboards/bastardkb/dilemma/4x6_4/keymaps/dcdilemma/keymap.c
@@ -309,7 +309,7 @@ void test_fin(tap_dance_state_t *state, void *user_data) {
 
 // Dynamic TD Function
 #define ACTION_TAP_DANCE_FN_ADVANCED_USER(user_fn_on_each_tap, user_fn_on_dance_finished, user_fn_on_dance_reset, usr_data) \
-        { .fn = {user_fn_on_each_tap, user_fn_on_dance_finished, user_fn_on_dance_reset}, .user_data =  &(void*)(usr_data), }
+        { .fn = {user_fn_on_each_tap, user_fn_on_dance_finished, user_fn_on_dance_reset}, .user_data =  (void*)(&usr_data), }
 // End dynamic TD Function
 
 // Tap Dance definitions
